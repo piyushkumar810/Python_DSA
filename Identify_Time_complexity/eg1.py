8f9c085d5deeb2e9019c784020260a1513d1d386
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-17 08:03:44 +0530

    Identify Time complexity

---
+++ Identify_Time_complexity/eg1.py
@@ -0,0 +1,23 @@
+def Print(n):
+    for i in range(n):
+        print("hello")
+
+Print(1)
+print("\n")
+
+Print(2)
+print("\n")
+
+Print(3)
+print("\n")
+
+Print(4)
+
+# if n=1 } it will print 1 time
+# if n-2 } it will print 2 time
+# if n-3 } it will print 3 time
+# if n-4 } it will print 4 time
+
+# so, we can say that 
+#            no of operation is directly proportional to n
+#            so, time complexity = O(n)
