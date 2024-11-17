cb1bfe1da7ba86477d4c1cb0b7ec58f401e1c8cd
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-17 08:06:28 +0530

    constant time

---
+++ Identify_Time_complexity/eg2.py
@@ -0,0 +1,17 @@
+def fun(n):
+    print("hello")
+
+fun(1)
+print("\n")
+
+fun(2)
+print("\n")
+
+fun(3)
+print("\n")
+
+fun(4)
+
+# you can see here no of operation is independent of n (input size)
+
+# so -> O(1)--> constant
