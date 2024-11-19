2e9c31a8b8f1a5a37198860b2347dbe01ebee5fa
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-19 15:45:47 +0530

    added new way

--- Interview_Questions/push_zeros_back.py
+++ Interview_Questions/push_zeros_back.py
@@ -16,3 +16,17 @@ def remove_zeros(my_list):
 
 my_list=[1,2,0,4,3,0,0,5,0] 
 print(remove_zeros(my_list))
+print("\n")
+
+# -------------------------------------------------or
+
+def push_zero_back(my_list1):
+    for i in my_list1:
+        if(i==0):
+            my_list1.remove(i)
+            my_list1.append(i)
+    return my_list1
+
+my_list1=[1,2,0,4,3,0,0,5,0]
+result=push_zero_back(my_list1)
+print(result)
