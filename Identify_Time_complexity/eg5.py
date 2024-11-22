983bcf9904b20d08532c01afc27db066eaacf5a3
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-22 08:26:44 +0530

    finding time and space complexity

---
+++ Identify_Time_complexity/eg5.py
@@ -0,0 +1,12 @@
+def fun(n):
+    i=1
+
+    while(i<n):
+        i=i*2
+        print("hello")
+
+fun(4)
+
+# time complexity = O(log n)
+
+# space complexity = O(1), because no extra space is taken in this problem
