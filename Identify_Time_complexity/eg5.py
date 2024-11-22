1da2b2c65745d4516e87ea71b2e34aff14acb019
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-22 08:38:39 +0530

    added

--- Identify_Time_complexity/eg5.py
+++ Identify_Time_complexity/eg5.py
@@ -10,3 +10,22 @@ fun(4)
 # time complexity = O(log n)
 
 # space complexity = O(1), because no extra space is taken in this problem
+
+# q2)
+def fun(n):
+    for i in range(n):
+        i=1
+
+        while(i<n):
+            i=i*2
+            print("hello")
+
+fun(4)
+
+# time complexity = 
+        #      for for loop = O(n)
+        #            for while part = O(log n)
+#            total= O(n) * O(log n)
+#             T.C = O(n log n)
+
+#             S.C = O(1), remains constant
