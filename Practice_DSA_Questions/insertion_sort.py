f52ea62db9d8937f70a00ff6195690f867d3b5a2
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-25 16:22:03 +0530

    insertion sort in python

--- Practice_DSA_Questions/insertion_sort.py
+++ Practice_DSA_Questions/insertion_sort.py
@@ -1,9 +1,20 @@
+# ------------------------------------insertion sort in python 
+
 def insertion_sort(arr):
     n=len(arr)
 
-    
+    for i in range(1,n):
+        key=arr[i]
 
-
+        # j is initialized to the index of the last element in the sorted portion of the array.
+        j=i-1
+        while j>=0 and key<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+        
+        arr[j+1]=key
+        
+    return arr
 
 arr=[10,2,5,14,-6]
 sorted_list=insertion_sort(arr)
