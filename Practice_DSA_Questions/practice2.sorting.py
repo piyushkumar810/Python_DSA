e5758cff63a03bcbd53f973fbcbb861a69d66d5c
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-25 15:26:47 +0530

    added

--- Practice_DSA_Questions/practice2.sorting.py
+++ Practice_DSA_Questions/practice2.sorting.py
@@ -13,6 +13,7 @@ def bubble_sort(arr):
 arr=[3,5,1,2,4]
 result=bubble_sort(arr)
 print(result)
+print("\n")
 
 # ---------------------------------------- selection sort
 
@@ -20,15 +21,16 @@ def selection_sort(arr):
     n1=len(arr1)
 
     for i in range(n1):
-        first_element=arr[i]
+        min_index=i
 
         for j in range(i+1,n1):
-            if(arr[j]<arr[j+1]):
-                smallest_element=j
+            if(arr[j]<arr[min_index]):
+                min_index=j
 
+        arr[i],arr[min_index]=arr[min_index],arr[i]
+    return arr
 
-
-
-arr1=[8,25,33,21,50,9,5,37,8,28]
+arr1=[38,25,33,21,50,9,5,37,8,28]
 result1=selection_sort(arr1)
 print(result1)
+print("\n")
