94a793c927d8969815247816076d2f39be103363
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-26 21:39:47 +0530

    TC O(n log n)

---
+++ Identify_Time_complexity/eg11.py
@@ -0,0 +1,13 @@
+def func(n):
+    i=1
+    while(i<n):
+        j=0
+        while(j<n):
+            j=j+1
+            print("hello")
+        i=i+1
+
+func(8)
+
+# time complexity
+        #   O(n log n)
