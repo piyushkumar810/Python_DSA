b53d7ad616a3d77fbabc0aa8ba1cd5a4361d9e73
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-12-13 14:42:05 +0530

    finonacci number

---
+++ Practice_DSA_Questions/recursion4_fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci_recursive(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+    
+number=int(input("enter the number "))
+result=fibonacci_recursive(number)
+print(result)
