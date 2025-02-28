0c40fdd12b5ce09f99c9bfeb3795646704fdcd2a
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-02-28 18:10:00 +0530

    depth traversal tree

---
+++ Practice_DSA_Questions/Tree_in_Pre_Post_triversal.py
@@ -0,0 +1,63 @@
+# -------------------------------------- Tree depth first search --------------------------------
+
+# in_order traversal
+# pre_order traversal
+# post_order traversal
+
+class Node:
+    def __init__(self,value):
+        self.val=value
+        self.left=None
+        self.right=None
+
+root=Node(1)
+root.left=Node(2)
+root.right=Node(3)
+root.left.left=Node(4)
+root.left.right=Node(5)
+
+def display(root):
+    if root is None:
+        return
+    else:
+        print(root.val)
+        display(root.left)
+        display(root.right)
+
+# print(display(root))
+print()
+
+# pre order traversal of tree (root -> left -> right)
+def pre_order(root):
+    if root is None:
+        return
+    else:
+        print(root.val , end=" ")
+        pre_order(root.left)
+        pre_order(root.right)
+
+print(pre_order(root))
+print()
+
+# in order traversal of tree (left -> root ->  right)
+def in_order(root):
+    if root is None:
+        return
+    else:
+        in_order(root.left)
+        print(root.val , end=" ")
+        in_order(root.right)
+
+print(in_order(root))
+print()
+
+# post order traversal of tree (left -> right -> root)
+def post_order(root):
+    if root is None:
+        return
+    else:
+        post_order(root.left)
+        post_order(root.right)
+        print(root.val , end=" ")
+
+print(post_order(root))
