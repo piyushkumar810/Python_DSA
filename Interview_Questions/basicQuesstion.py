66fcd676673c42465e27e1ebe88cd95d35f04cd7
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-05-14 16:33:05 +0530

    QUESTIONS

---
+++ Interview_Questions/basicQuesstion.py
@@ -0,0 +1,9 @@
+# What is a correct syntax to add the value 'Hello World', to 3 variables in one statement?
+
+# Insert the correct syntax to assign values to multiple variables in one line:
+x,y,z = "Orange", "Banana", "Cherry"
+
+#--------- QUESTION 1 ---------
+fruits = ['apple', 'banana', 'cherry']
+a, b, c = fruits
+print(a)
